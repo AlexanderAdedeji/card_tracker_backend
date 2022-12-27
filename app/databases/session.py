21ec208e5d3a0cd1e1ac4818ea9f54256ef88ca1
Alexander <alexadedeji15@gmail.com>
2022-12-27 17:02:01 +0000

    added startup.sh

--- app/databases/session.py
+++ app/databases/session.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from app.core.settings.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL= f"postgresql://cardportalapi_admin@card-portal-api-server:CardTrackingPortal2022@card-portal-api-server.postgres.database.azure.com:{settings.DATABASE_PORT}/card-portal-api-db"
+
+
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
